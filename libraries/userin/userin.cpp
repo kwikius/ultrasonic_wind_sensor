2ac189fe5b169d18c2d51e7c9a6e42df77c14b17
Andy Little <kwikius@yahoo.com>
2021-08-29 17:31:51 +0100

    user_in: fix some bugs. extend max password size. remove some unused global functions

--- libraries/userin/userin.cpp
+++ libraries/userin/userin.cpp
@@ -4,9 +4,10 @@
 #include <builtin_led.h>
 
 namespace {
+  
    // Replace with your network credentials
-   constexpr size_t max_ssid = 20;
-   constexpr size_t max_password = 20;
+   constexpr size_t max_ssid = 128;
+   constexpr size_t max_password = 128;
 
    char ssid[max_ssid] = {0};
    char password[max_password] = {0};
@@ -14,21 +15,23 @@ namespace {
 }
 
 const char* get_wifi_network_ssid() { return ssid;}
-  const char* get_wifi_password()  { return password;}
+const char* get_wifi_password()  { return password;}
+
+ //  void flush_serial_input_buffer()
+
+namespace {
 
    void flush_serial_input_buffer()
    {
      while (Serial.available()){
-       int ch = Serial.read();
-       (void)ch;
+        int ch = Serial.read();
+        (void)ch;
      }
    }
 
-namespace {
-
-   constexpr char eeprom_magic[] = "XUHBoFI";
-   constexpr size_t eeprom_magic_size = sizeof(eeprom_magic)/sizeof(eeprom_magic[0]);
-
+   constexpr char eeprom_magic[] = "bXUHBoFI2";
+   constexpr size_t eeprom_magic_size = 
+      sizeof(eeprom_magic)/sizeof(eeprom_magic[0]);
 
    int get_user_string( char* buf, size_t buf_len)
    {
@@ -54,169 +57,167 @@ namespace {
        return 0;
    }
 
-
-
-
-bool get_eeprom_network_params()
-{
-   //in ESP2866 EEPROM.begin() reads data from flash to a ram buffer.
-   // After that the data is read from the buffer
-   EEPROM.begin(eeprom_magic_size + max_ssid + max_password);
-   char buf[eeprom_magic_size];
-   for ( size_t i = 0U; i < eeprom_magic_size; ++i){
-     buf[i] = EEPROM.read(i);
-   }
-   
-   if (strncmp(buf,eeprom_magic,eeprom_magic_size) == 0){
-      size_t start = eeprom_magic_size;
-      bool ok = false;
-      for (size_t i = 0; i < max_ssid; ++i){
-        char ch = EEPROM.read(start + i);
-        if ( ch == '\0'){
-            if ( i > 0){
-               ssid[i] = ch;
-               ok = true;
-               break;
-            }else{
-              Serial.println("invalid SSID in eeprom : STOP"); for(;;){;}
-            }
-        }else{
-          ssid[i] = ch;
-        }
+   bool get_eeprom_network_params()
+   {
+      //in ESP2866 EEPROM.begin() reads data from flash to a ram buffer.
+      // After that the data is read from the buffer
+      EEPROM.begin(eeprom_magic_size + max_ssid + max_password);
+      char buf[eeprom_magic_size];
+      for ( size_t i = 0U; i < eeprom_magic_size; ++i){
+        buf[i] = EEPROM.read(i);
       }
-      if (ok == false){Serial.println("SSID too long in eeprom : STOP"); for(;;){;} }
       
-      start = 8 + max_ssid;
-      ok = false;
-       for (size_t i = 0; i < max_password; ++i){
-        char ch = EEPROM.read(start + i);
-        if ( ch == '\0'){
-            if ( i > 0){
-               ssid[i] = ch;
-               ok = true; 
-               break;
-            }else{
-              Serial.println("invalid info in eeprom : STOP"); for(;;){;}
-            }
-        }else{
-          password[i] = ch;
-        }
+      if (strncmp(buf,eeprom_magic,eeprom_magic_size) == 0){
+         size_t start = eeprom_magic_size;
+         bool ok = false;
+         for (size_t i = 0; i < max_ssid; ++i){
+           char ch = EEPROM.read(start + i);
+           if ( ch == '\0'){
+               if ( i > 0){
+                  ssid[i] = ch;
+                  ok = true;
+                  break;
+               }else{
+                 Serial.println("invalid SSID in eeprom : STOP"); for(;;){;}
+               }
+           }else{
+             ssid[i] = ch;
+           }
+         }
+         if (ok == false){Serial.println("SSID too long in eeprom : STOP"); for(;;){;} }
+         
+         start = eeprom_magic_size + max_ssid;
+         ok = false;
+          for (size_t i = 0; i < max_password; ++i){
+           char ch = EEPROM.read(start + i);
+           if ( ch == '\0'){
+               if ( i > 0){
+                  password[i] = ch;
+                  ok = true; 
+                  break;
+               }else{
+                 Serial.println("invalid Pswd in eeprom : STOP"); for(;;){;}
+               }
+           }else{
+             password[i] = ch;
+           }
+         }
+         if (ok == false){Serial.println("bad info in eeprom : STOP"); for(;;){;} }
+         //N.B. Dont use EEPROM.end() when reading
+         // as it will cause a write to flash
+         return true;
+      }else{ // invalid magic
+         return false;
       }
-      if (ok == false){Serial.println("bad info in eeprom : STOP"); for(;;){;} }
-      //N.B. Dont use EEPROM.end() when reading
-      // as it will cause a write to flash
-      return true;
-   }else{ // invalid magic
-      return false;
    }
-}
 
-bool write_eeprom_network_params()
-{
-    EEPROM.begin(50);
-    size_t start = 0;
-    for (size_t i = 0U; i < eeprom_magic_size; ++i){
-       EEPROM.write(start + i, eeprom_magic[i]);
-    }
-    start += eeprom_magic_size;
-    for (size_t i = 0U; i < max_ssid; ++i){
-       EEPROM.write(start + i, ssid[i]);
-    }
-    start += max_ssid;
-    for (size_t i = 0U; i < max_password; ++i){
-       EEPROM.write(start + i, password[i]);
-    }
-    //###################################
-    //N.B. Must call end to write the data to flash
-    // prior it is only held in a ram cache
-
-    EEPROM.end();
-    //######################################
-    return true;
-}
-
-bool get_manual_network_params()
-{
-      for (;;){
-       Serial.print("Enter ssid: ");
-       int ret = get_user_string(ssid,max_ssid);
-       if (ret < 0){
-         Serial.print("Error from get ssid : STOP!\n"); for(;;){;}
+   bool write_eeprom_network_params()
+   {
+       EEPROM.begin(eeprom_magic_size + max_ssid + max_password);
+       size_t start = 0;
+       for (size_t i = 0U; i < eeprom_magic_size; ++i){
+          EEPROM.write(start + i, eeprom_magic[i]);
        }
-       if ( ret > 0) {
-         Serial.println(ssid);
-         break;
+       start += eeprom_magic_size;
+       for (size_t i = 0U; i < max_ssid; ++i){
+          EEPROM.write(start + i, ssid[i]);
+       }
+       start += max_ssid;
+       for (size_t i = 0U; i < max_password; ++i){
+          EEPROM.write(start + i, password[i]);
+       }
+       //###################################
+       //N.B. Must call end to write the data to flash
+       // prior it is only held in a ram cache
+
+       EEPROM.end();
+       //######################################
+       return true;
+   }
+
+   bool get_manual_network_params()
+   {
+         for (;;){
+          Serial.print("Enter ssid: ");
+          int ret = get_user_string(ssid,max_ssid);
+          if (ret < 0){
+            Serial.print("Error from get ssid : STOP!\n"); for(;;){;}
+          }
+          if ( ret > 0) {
+            Serial.println(ssid);
+            break;
+          }else {
+              Serial.println("");
+          }
+         }
+     
+         for (;;){
+          Serial.print("Enter passwd: ");
+          int ret = get_user_string(password,max_password);
+          if (ret < 0){
+            Serial.print("Error from get password : STOP!\n"); for(;;){;}
+          }
+          if ( ret > 0) {
+            Serial.println("password");
+            break;
+          } else {
+              Serial.println("");
+          }
+         }
+       return true;
+   }
+
+   bool do_setup()
+   {
+       while (! get_manual_network_params()) {;}
+       flush_serial_input_buffer();
+       Serial.println("Save Settings(y\\n)? ");
+       while (! Serial.available()){;}
+       char ch = Serial.read();
+       if (( ch == 'y' ) || (ch == 'Y')){
+          Serial.println("y");
+          return write_eeprom_network_params();
        }else {
-           Serial.println("");
+          Serial.println("n");
+          return true;
        }
-      }
-  
-      for (;;){
-       Serial.print("Enter passwd: ");
-       int ret = get_user_string(password,max_password);
-       if (ret < 0){
-         Serial.print("Error from get ssid : STOP!\n"); for(;;){;}
-       }
-       if ( ret > 0) {
-         Serial.println("password");
-         break;
-       } else {
-           Serial.println("");
-       }
-      }
-    return true;
-}
+   }
+
+   bool get_network_params()
+   {
+     if ( ! get_eeprom_network_params()){
+         Serial.println("No settings found, Please enter manually");
+         return do_setup();
+     }
+     return true;
+   }
 
 }  // ~namespace
 
-bool do_setup()
-{
-    while (! get_manual_network_params()) {;}
-    flush_serial_input_buffer();
-    Serial.println("Save Settings(y\\n)? ");
-    while (! Serial.available()){;}
-    char ch = Serial.read();
-    if (( ch == 'y' ) || (ch == 'Y')){
-       Serial.println("y");
-       return write_eeprom_network_params();
-    }else {
-       Serial.println("n");
-       return true;
-    }
-}
 
-bool get_network_params()
+void setup_network_params()
 {
-  if ( ! get_eeprom_network_params()){
-      Serial.println("No settings found, Please enter manually");
-      return do_setup();
-  }
-  return true;
-}
-
-bool check_do_setup()
-{
-  flush_serial_input_buffer();
-
   auto now = millis();
   int rets = 0;
   auto last_led_switch = now;
-  //delay here so that  commands can be entered
-  //eg to change network and passwd
+  //delay here so that user can change network ssid and passwd if wants
+  flush_serial_input_buffer();
   Serial.println("Press ret x 3 for setup");
   while ( (millis() - now) < 5000){
-    if ( Serial.available()){
-        int ch = Serial.read();
-        if ( ch == '\r'){
-          if (++rets > 2){
-             return do_setup();
+    if ( Serial.available() && ( Serial.read() == '\r' ) ){
+       if (++rets > 2){
+          if(do_setup()){
+             return;
+          }else{
+             break;
           }
-        }
+       }
     }
     if ( (millis() - last_led_switch) >= 1000){
       last_led_switch = millis();
       complement_builtin_led();
     }
   }
-  return false;
+  // params should now be in eeprom, if not ask user
+  while (! get_network_params() ) { asm volatile("nop":::);}
 }
--- libraries/userin/userin.h
+++ libraries/userin/userin.h
@@ -1,22 +1,23 @@
 #ifndef ARDUINO_ESP8266_LIBRARIES_USER_IN_H_INCLUDED
 #define ARDUINO_ESP8266_LIBRARIES_USER_IN_H_INCLUDED
 
-/*
-   -1 error
-   0 good
-   above 0 other error
-   
-   int get_network_params( network_params & 
-*/
+/**
+* @brief Send message re user input on serial port.
+*  Wait for 5 secs to see if user has pressed return X3
+*  If so get network data from user
+*  else get settings from eeprom if available
+* @return 
+**/
+void setup_network_params();
 
+/**
+* @brief get the network SSID
+**/
 const char* get_wifi_network_ssid();
+
+/**
+* @brief get the network password
+**/
 const char* get_wifi_password();
-
-bool get_network_params();
-
-void flush_serial_input_buffer();
-
-bool do_setup();
-bool check_do_setup();
 
 #endif
