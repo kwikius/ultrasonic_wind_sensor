6ff6c498fc725d0aed08508c94834321c1999a2b
Andy Little <kwikius@yahoo.com>
2021-10-20 07:55:35 +0100

    include wind_sensor_packet per https://github.com/kwikius/ultrasonic_wind_sensor/issues/1

---
+++ libraries/windsensor_packet/windsensor_packet.h
@@ -0,0 +1,51 @@
+#ifndef WINDSENSOR_WIND_SENSOR_PACKET_HPP_INCLUDED
+#define WINDSENSOR_WIND_SENSOR_PACKET_HPP_INCLUDED
+
+#include <quan/tracker/zapp4/crc.hpp>
+#include <quan/tracker/zapp4/do_crc.hpp>
+#include <quan/tracker/zapp4/decode_packet.hpp>
+#include <quan/uav/cobs/static_packet_parser.hpp>
+#include <quan/velocity.hpp>
+#include <quan/angle.hpp>
+
+ enum class PacketID : uint16_t {
+   WindSpeedAndDirection = 1U,
+   Undef = 2U
+ };
+
+ union velocity_and_direction_packet{
+      // seed must be less than            4294967295                                 
+      static constexpr uint32_t crc_seed = 1141577654U;
+      static constexpr PacketID packet_id = PacketID::WindSpeedAndDirection;
+
+      velocity_and_direction_packet(quan::velocity_<float>::m_per_s const &speed, quan::angle_<float>::deg const & direction)
+      : values{speed,direction}{ quan::tracker::zapp4::do_crc(*this);}
+      struct packet{
+          packet(quan::velocity_<float>::m_per_s const &speed_in, quan::angle_<float>::deg const & direction_in)
+          :id{packet_id},
+          reserved{0U},
+          speed{speed_in},
+          direction{direction_in}
+          {}
+
+          PacketID id;
+          uint16_t reserved;
+          quan::velocity_<float>::m_per_s speed;
+          quan::angle_<float>::deg direction;
+          uint32_t crc;
+       } values;
+       uint8_t ar[sizeof(values)];
+
+      template <uint16_t MaxEncodedPacketLength>
+      velocity_and_direction_packet(quan::uav::cobs::static_packet_parser<MaxEncodedPacketLength> const & parser)
+      {
+         static_assert(MaxEncodedPacketLength > sizeof(ar),"packet_parser buffer too short");
+         memcpy(ar,parser.get_decoded_packet(),sizeof(ar));
+      }
+ };
+
+static_assert ( sizeof (velocity_and_direction_packet) == 16,"unexpected union size");
+static_assert ( (sizeof (velocity_and_direction_packet) % 4) == 0,"unexpected union size");
+
+
+#endif // WINDSENSOR_WIND_SENSOR_PACKET_HPP_INCLUDED
